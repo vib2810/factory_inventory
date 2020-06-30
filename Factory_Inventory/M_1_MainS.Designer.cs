52f31f878b34b9707ee2fad30322f569584ae1f5
Gaurang Mohta <gaumo62@iitkgp.ac.in>
2020-06-30 13:45:06 +0530

    added addendance menu

--- Factory_Inventory/M_1_MainS.Designer.cs
+++ Factory_Inventory/M_1_MainS.Designer.cs
@@ -47,15 +47,16 @@ namespace Factory_Inventory
             this.inventoryUC = new Factory_Inventory.M_1_inventoryUC();
             this.vouchersUC = new Factory_Inventory.M_1_vouchersUC();
             this.AttendanceButton = new System.Windows.Forms.Button();
+            this.qualityTableAdapter1 = new Factory_Inventory.FactoryDataDataSetTableAdapters.QualityTableAdapter();
             this.SuspendLayout();
             // 
             // logOutButton
             // 
             this.logOutButton.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.logOutButton.BackColor = System.Drawing.Color.Red;
-            this.logOutButton.Location = new System.Drawing.Point(704, 45);
+            this.logOutButton.Location = new System.Drawing.Point(737, 12);
             this.logOutButton.Name = "logOutButton";
-            this.logOutButton.Size = new System.Drawing.Size(111, 38);
+            this.logOutButton.Size = new System.Drawing.Size(111, 58);
             this.logOutButton.TabIndex = 1;
             this.logOutButton.Text = "Log Out";
             this.logOutButton.UseVisualStyleBackColor = false;
@@ -65,7 +66,7 @@ namespace Factory_Inventory
             // 
             this.newUserButton.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.newUserButton.BackColor = System.Drawing.Color.DarkGray;
-            this.newUserButton.Location = new System.Drawing.Point(704, 406);
+            this.newUserButton.Location = new System.Drawing.Point(737, 475);
             this.newUserButton.Name = "newUserButton";
             this.newUserButton.Size = new System.Drawing.Size(111, 66);
             this.newUserButton.TabIndex = 9;
@@ -77,9 +78,9 @@ namespace Factory_Inventory
             // 
             this.vouchersButton.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.vouchersButton.BackColor = System.Drawing.Color.DarkGray;
-            this.vouchersButton.Location = new System.Drawing.Point(704, 89);
+            this.vouchersButton.Location = new System.Drawing.Point(737, 76);
             this.vouchersButton.Name = "vouchersButton";
-            this.vouchersButton.Size = new System.Drawing.Size(111, 38);
+            this.vouchersButton.Size = new System.Drawing.Size(111, 58);
             this.vouchersButton.TabIndex = 3;
             this.vouchersButton.Text = "&Vouchers";
             this.vouchersButton.UseVisualStyleBackColor = false;
@@ -89,9 +90,9 @@ namespace Factory_Inventory
             // 
             this.inventoryButton.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.inventoryButton.BackColor = System.Drawing.Color.DarkGray;
-            this.inventoryButton.Location = new System.Drawing.Point(704, 133);
+            this.inventoryButton.Location = new System.Drawing.Point(737, 140);
             this.inventoryButton.Name = "inventoryButton";
-            this.inventoryButton.Size = new System.Drawing.Size(111, 38);
+            this.inventoryButton.Size = new System.Drawing.Size(111, 58);
             this.inventoryButton.TabIndex = 4;
             this.inventoryButton.Text = "&Inventory";
             this.inventoryButton.UseVisualStyleBackColor = false;
@@ -101,9 +102,9 @@ namespace Factory_Inventory
             // 
             this.loginLogButton.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.loginLogButton.BackColor = System.Drawing.Color.DarkGray;
-            this.loginLogButton.Location = new System.Drawing.Point(704, 229);
+            this.loginLogButton.Location = new System.Drawing.Point(737, 268);
             this.loginLogButton.Name = "loginLogButton";
-            this.loginLogButton.Size = new System.Drawing.Size(111, 38);
+            this.loginLogButton.Size = new System.Drawing.Size(111, 58);
             this.loginLogButton.TabIndex = 6;
             this.loginLogButton.Text = "Login Log";
             this.loginLogButton.UseVisualStyleBackColor = false;
@@ -113,7 +114,7 @@ namespace Factory_Inventory
             // 
             this.UsersButton.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.UsersButton.BackColor = System.Drawing.Color.DarkGray;
-            this.UsersButton.Location = new System.Drawing.Point(704, 334);
+            this.UsersButton.Location = new System.Drawing.Point(737, 403);
             this.UsersButton.Name = "UsersButton";
             this.UsersButton.Size = new System.Drawing.Size(111, 66);
             this.UsersButton.TabIndex = 8;
@@ -127,7 +128,7 @@ namespace Factory_Inventory
             this.usernameLabel.AutoSize = true;
             this.usernameLabel.Cursor = System.Windows.Forms.Cursors.Default;
             this.usernameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.usernameLabel.Location = new System.Drawing.Point(12, 10);
+            this.usernameLabel.Location = new System.Drawing.Point(29, 38);
             this.usernameLabel.Name = "usernameLabel";
             this.usernameLabel.Size = new System.Drawing.Size(99, 32);
             this.usernameLabel.TabIndex = 0;
@@ -137,9 +138,9 @@ namespace Factory_Inventory
             // 
             this.backupRestoreButton.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.backupRestoreButton.BackColor = System.Drawing.Color.DarkGray;
-            this.backupRestoreButton.Location = new System.Drawing.Point(704, 177);
+            this.backupRestoreButton.Location = new System.Drawing.Point(737, 204);
             this.backupRestoreButton.Name = "backupRestoreButton";
-            this.backupRestoreButton.Size = new System.Drawing.Size(111, 46);
+            this.backupRestoreButton.Size = new System.Drawing.Size(111, 58);
             this.backupRestoreButton.TabIndex = 5;
             this.backupRestoreButton.Text = "&Backup and Restore";
             this.backupRestoreButton.UseVisualStyleBackColor = false;
@@ -152,7 +153,7 @@ namespace Factory_Inventory
             this.label4.BackColor = System.Drawing.Color.Gainsboro;
             this.label4.Cursor = System.Windows.Forms.Cursors.Default;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(794, 206);
+            this.label4.Location = new System.Drawing.Point(827, 222);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(17, 17);
             this.label4.TabIndex = 0;
@@ -165,7 +166,7 @@ namespace Factory_Inventory
             this.label2.BackColor = System.Drawing.Color.Gainsboro;
             this.label2.Cursor = System.Windows.Forms.Cursors.Default;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(800, 150);
+            this.label2.Location = new System.Drawing.Point(833, 177);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(11, 17);
             this.label2.TabIndex = 0;
@@ -178,7 +179,7 @@ namespace Factory_Inventory
             this.label1.BackColor = System.Drawing.Color.Gainsboro;
             this.label1.Cursor = System.Windows.Forms.Cursors.Default;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(794, 106);
+            this.label1.Location = new System.Drawing.Point(827, 113);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(17, 17);
             this.label1.TabIndex = 0;
@@ -187,7 +188,7 @@ namespace Factory_Inventory
             // m_1_BackupRestoreUC
             // 
             this.m_1_BackupRestoreUC.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.m_1_BackupRestoreUC.Location = new System.Drawing.Point(12, 45);
+            this.m_1_BackupRestoreUC.Location = new System.Drawing.Point(29, 73);
             this.m_1_BackupRestoreUC.Name = "m_1_BackupRestoreUC";
             this.m_1_BackupRestoreUC.Size = new System.Drawing.Size(671, 427);
             this.m_1_BackupRestoreUC.TabIndex = 0;
@@ -195,7 +196,7 @@ namespace Factory_Inventory
             // loginlogUC
             // 
             this.loginlogUC.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.loginlogUC.Location = new System.Drawing.Point(18, 44);
+            this.loginlogUC.Location = new System.Drawing.Point(35, 72);
             this.loginlogUC.Name = "loginlogUC";
             this.loginlogUC.Size = new System.Drawing.Size(671, 427);
             this.loginlogUC.TabIndex = 0;
@@ -203,7 +204,7 @@ namespace Factory_Inventory
             // usersUC
             // 
             this.usersUC.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.usersUC.Location = new System.Drawing.Point(18, 45);
+            this.usersUC.Location = new System.Drawing.Point(35, 73);
             this.usersUC.Name = "usersUC";
             this.usersUC.Size = new System.Drawing.Size(671, 427);
             this.usersUC.TabIndex = 0;
@@ -211,7 +212,7 @@ namespace Factory_Inventory
             // inventoryUC
             // 
             this.inventoryUC.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.inventoryUC.Location = new System.Drawing.Point(18, 45);
+            this.inventoryUC.Location = new System.Drawing.Point(35, 73);
             this.inventoryUC.Name = "inventoryUC";
             this.inventoryUC.Size = new System.Drawing.Size(671, 427);
             this.inventoryUC.TabIndex = 0;
@@ -219,7 +220,7 @@ namespace Factory_Inventory
             // vouchersUC
             // 
             this.vouchersUC.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.vouchersUC.Location = new System.Drawing.Point(18, 45);
+            this.vouchersUC.Location = new System.Drawing.Point(35, 73);
             this.vouchersUC.Name = "vouchersUC";
             this.vouchersUC.Size = new System.Drawing.Size(671, 427);
             this.vouchersUC.TabIndex = 0;
@@ -228,18 +229,22 @@ namespace Factory_Inventory
             // 
             this.AttendanceButton.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.AttendanceButton.BackColor = System.Drawing.Color.DarkGray;
-            this.AttendanceButton.Location = new System.Drawing.Point(704, 273);
+            this.AttendanceButton.Location = new System.Drawing.Point(737, 332);
             this.AttendanceButton.Name = "AttendanceButton";
-            this.AttendanceButton.Size = new System.Drawing.Size(111, 38);
+            this.AttendanceButton.Size = new System.Drawing.Size(111, 58);
             this.AttendanceButton.TabIndex = 7;
             this.AttendanceButton.Text = "Attendance";
             this.AttendanceButton.UseVisualStyleBackColor = false;
+            // 
+            // qualityTableAdapter1
+            // 
+            this.qualityTableAdapter1.ClearBeforeFill = true;
             // 
             // M_1_MainS
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(848, 496);
+            this.ClientSize = new System.Drawing.Size(882, 553);
             this.Controls.Add(this.AttendanceButton);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
@@ -288,5 +293,6 @@ namespace Factory_Inventory
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button AttendanceButton;
+        private FactoryDataDataSetTableAdapters.QualityTableAdapter qualityTableAdapter1;
     }
 }
--- Factory_Inventory/TwistERP.Designer.cs
+++ Factory_Inventory/TwistERP.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.openWindowsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eRPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.attendanceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -37,25 +39,42 @@
             // 
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.openWindowsToolStripMenuItem});
+            this.openWindowsToolStripMenuItem,
+            this.eRPToolStripMenuItem,
+            this.attendanceToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.MdiWindowListItem = this.openWindowsToolStripMenuItem;
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 30);
+            this.menuStrip1.Size = new System.Drawing.Size(982, 28);
             this.menuStrip1.TabIndex = 3;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // openWindowsToolStripMenuItem
             // 
+            this.openWindowsToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
             this.openWindowsToolStripMenuItem.Name = "openWindowsToolStripMenuItem";
             this.openWindowsToolStripMenuItem.Size = new System.Drawing.Size(124, 24);
             this.openWindowsToolStripMenuItem.Text = "Open Windows";
+            // 
+            // eRPToolStripMenuItem
+            // 
+            this.eRPToolStripMenuItem.Name = "eRPToolStripMenuItem";
+            this.eRPToolStripMenuItem.Size = new System.Drawing.Size(48, 24);
+            this.eRPToolStripMenuItem.Text = "ERP";
+            this.eRPToolStripMenuItem.Click += new System.EventHandler(this.eRPToolStripMenuItem_Click);
+            // 
+            // attendanceToolStripMenuItem
+            // 
+            this.attendanceToolStripMenuItem.Name = "attendanceToolStripMenuItem";
+            this.attendanceToolStripMenuItem.Size = new System.Drawing.Size(99, 24);
+            this.attendanceToolStripMenuItem.Text = "Attendance";
+            this.attendanceToolStripMenuItem.Click += new System.EventHandler(this.attendanceToolStripMenuItem_Click);
             // 
             // TwistERP
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(982, 753);
             this.Controls.Add(this.menuStrip1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -75,5 +94,7 @@
 
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem openWindowsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eRPToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem attendanceToolStripMenuItem;
     }
 }
