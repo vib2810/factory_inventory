a0d7c8182f54d79ec2fcb80861a53ddf1dc6e976
Officei3 <Officei3@mohta.com>
2020-07-16 12:54:33 +0530

    ran to run in office in Polyester

--- queries/To Run In Office/To run in office.sql
+++ queries/To Run In Office/To run in office.sql
@@ -1,5 +1,5 @@
 use FactoryData
 
---RUN IN Polyester: N		Cotton: N
+--RUN IN Polyester: Y		Cotton: N
 alter table Sales_Voucher drop column Carton_No_Arr;
 alter table Sales_Voucher drop column Carton_Fiscal_Year;
