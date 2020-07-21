4bc11ab08854f7f543aca3d843ab4fed397a1d7c
office pentium <officepentium@gmail.com>
2020-07-21 12:55:48 +0530

    ran in cotton

--- queries/To Run In Office/To run in office.sql
+++ queries/To Run In Office/To run in office.sql
@@ -1,10 +1,10 @@
 use FactoryData
 
---RUN IN Polyester: Y 15-07		Cotton: N
+--RUN IN Polyester: Y 15-07		Cotton: Y 21-07
 --alter table Sales_Voucher drop column Carton_No_Arr;
 --alter table Sales_Voucher drop column Carton_Fiscal_Year;
 
---RUN IN Polyester: N Cotton: N
+--RUN IN Polyester: N Cotton: Cotton: Y 21-07
 --create table Opening_Stock
 --(
 --	Voucher_ID int NOT NULL Primary Key Identity(1,1),
@@ -12,13 +12,14 @@ use FactoryData
 --	Voucher_Name varchar(50) NOT NULL
 --);
 
-alter table Carton_Produced alter column Batch_No_Arr text NULL;
-alter table Carton_Produced alter column Dyeing_Company_Name varchar(50) NULL;
-alter table Carton_Produced alter column Carton_Weight decimal(7, 3) NULL;
-alter table Carton_Produced alter column Number_Of_Cones int NULL;
-alter table Carton_Produced alter column Cone_Weight decimal(6, 3) NULL;
-alter table Carton_Produced alter column Gross_Weight decimal(7, 3) NULL;
-alter table Carton_Produced alter column Batch_Fiscal_Year_Arr text NULL;
+--RUN IN Polyester: N Cotton: Cotton: Y 21-07
+--alter table Carton_Produced alter column Batch_No_Arr text NULL;
+--alter table Carton_Produced alter column Dyeing_Company_Name varchar(50) NULL;
+--alter table Carton_Produced alter column Carton_Weight decimal(7, 3) NULL;
+--alter table Carton_Produced alter column Number_Of_Cones int NULL;
+--alter table Carton_Produced alter column Cone_Weight decimal(6, 3) NULL;
+--alter table Carton_Produced alter column Gross_Weight decimal(7, 3) NULL;
+--alter table Carton_Produced alter column Batch_Fiscal_Year_Arr text NULL;
 
 
 
