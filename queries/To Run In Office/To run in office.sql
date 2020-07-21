c0e9dd666961e2293e415b81c1105591d610e70b
office pentium <officepentium@gmail.com>
2020-07-21 16:39:56 +0530

    changes added to office pentium

--- queries/To Run In Office/To run in office.sql
+++ queries/To Run In Office/To run in office.sql
@@ -1,10 +1,10 @@
 use FactoryData
 
---RUN IN Polyester: Y 15-07		Cotton: N
+--RUN IN Polyester: Y 15-07		Cotton: Y 21-07
 --alter table Sales_Voucher drop column Carton_No_Arr;
 --alter table Sales_Voucher drop column Carton_Fiscal_Year;
 
-----RUN IN Polyester: N Cotton: N
+----RUN IN Polyester: Y 21-07 Cotton: Y 21-07
 --create table Opening_Stock
 --(
 --	Voucher_ID int NOT NULL Primary Key Identity(1,1),
@@ -13,7 +13,7 @@ use FactoryData
 --	Deleted tinyint NULL
 --);
 
-------RUN IN Polyester: N Cotton: N
+------RUN IN Polyester: Y 21-07 Cotton: Y 21-07
 --alter table Carton_Produced alter column Batch_No_Arr text NULL;
 --alter table Carton_Produced alter column Dyeing_Company_Name varchar(50) NULL;
 --alter table Carton_Produced alter column Carton_Weight decimal(7, 3) NULL;
@@ -22,8 +22,8 @@ use FactoryData
 --alter table Carton_Produced alter column Gross_Weight decimal(7, 3) NULL;
 --alter table Carton_Produced alter column Batch_Fiscal_Year_Arr text NULL;
 
---RUN IN Polyester: N Cotton: N
---alter table Opening_Stock add Deleted tinyint NULL
+--RUN IN Polyester: N Cotton: Y 21-07
+alter table Opening_Stock add Deleted tinyint NULL
 alter table Carton_Produced add Opening_Voucher_ID int NULL
 
 
