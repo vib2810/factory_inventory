08813157f8e6789f0fbf56644a10b785ea475ab8
Gaurang Mohta <gaumo62@iitkgp.ac.in>
2020-08-05 18:19:06 +0530

    added search in table procedure in to run in office

--- queries/To Run In Office/To run in office.sql
+++ queries/To Run In Office/To run in office.sql
@@ -23,7 +23,58 @@ use FactoryData
 --alter table Carton_Produced alter column Batch_Fiscal_Year_Arr text NULL;
 
 --RUN IN Polyester: Y 23-07 Cotton: Y 21-07
-alter table Opening_Stock add Deleted tinyint NULL
-alter table Carton_Produced add Opening_Voucher_ID int NULL
+--alter table Opening_Stock add Deleted tinyint NULL
+--alter table Carton_Produced add Opening_Voucher_ID int NULL
+
+--RUN IN Polyester: N Cotton: N
+GO
+/****** Object:  StoredProcedure [dbo].[SearchInTable]    Script Date: 05-08-2020 18:16:25 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+--use FactoryData
+CREATE PROCEDURE [dbo].[SearchInTable] @tableName nvarchar(50), @searchText nvarchar(50), @date tinyint
+AS
+SET NOCOUNT ON;
+DECLARE @columnName NVARCHAR(100)
+DECLARE @sql NVARCHAR(1000) = 'SELECT * FROM ' + @tableName +' WHERE '
+
+DECLARE columns CURSOR FOR
+SELECT sys.columns.name FROM sys.tables
+INNER JOIN sys.columns ON sys.columns.object_id = sys.tables.object_id
+WHERE sys.tables.name = @tableName
+
+OPEN columns
+FETCH NEXT FROM columns
+INTO @columnName
+
+WHILE @@FETCH_STATUS = 0
+
+BEGIN
+	if (@columnName not like '%voucher%') and (@columnName not like '%fiscal%') and (@columnName not like '%deleted%')
+    begin
+		if(@date = 1)
+		begin
+			if @columnName like '%date%' 
+			begin SET @sql = @sql + @columnName + ' LIKE ''%' + @searchText + '%'' OR ' end
+		end
+		else
+		begin
+			if @columnName not like '%date%' 
+			begin SET @sql = @sql + @columnName + ' LIKE ''%' + @searchText + '%'' OR ' end
+		end
+	end
+    FETCH NEXT FROM columns
+    INTO @columnName    
+END
+
+CLOSE columns;    
+DEALLOCATE columns;
+
+SET @sql = LEFT(RTRIM(@sql), LEN(@sql) - 2)
+--select @sql
+EXEC(@sql)
+
 
 
