e267e2611b0fbc593bb3cd826e3fa67fda2e2e19
Gaurang Kaushik Mohta <gaumo62@iitkgp.ac.in>
2021-08-20 17:49:46 +0530

    delete firm option added (doesnt delete from sql yet)

--- Factory_Inventory/Main/Login.Designer.cs
+++ Factory_Inventory/Main/Login.Designer.cs
@@ -135,7 +135,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
         public System.Windows.Forms.RichTextBox firmTB;
     }
